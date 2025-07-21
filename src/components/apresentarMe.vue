48eb6e6461b68146cebb820996e4e0cc25d04159
ftreitasyuri <yuriftq@gmail.com>
2025-07-21 20:21:51 -0300

    Upload: Subindo final apresentarme

--- src/components/apresentarMe.vue
+++ src/components/apresentarMe.vue
@@ -2,7 +2,7 @@
 
     <main>
         <div class="bg-primary flex flex-col items-center mt-10 p-10">
-            <img alt="Foto de Perfil" class="rounded-full w-[20vw] h-[20vh] mb-10 border-5 border-gray-50"
+            <img alt="Foto de Perfil" class="rounded-full w-[10vw] h-[20vh] mb-10 border-5 border-gray-50"
              src="@/assets/images/perfil.jpg" width="125" height="125" />
             <span class="text-4xl">Yuri Freitas</span>
             <span class="text-fifth">Desenvolvedor Full Stack (JavaScript e PHP)</span>
