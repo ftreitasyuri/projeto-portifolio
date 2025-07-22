2472a643927e0822025d092ebf349b5a4b34ae02
ftreitasyuri <yuriftq@gmail.com>
2025-07-22 16:59:09 -0300

    Upload: Subindo config mobile first

--- src/components/apresentarMe.vue
+++ src/components/apresentarMe.vue
@@ -1,20 +1,21 @@
 <template>
 
     <main>
-        <div class="flex flex-col items-center mt-10 -mb-80 p-10 ">
-            <img alt="Foto de Perfil" class="rounded-full w-[15vw] h-[30vh] mb-10 border-5 border-gray-50"
+        <div class="flex flex-col gap-6 items-center mt-10  p-10">
+            <img alt="Foto de Perfil" class="rounded-full w-[65vw] sm:w-[15vw] h-[30vh] mb-10 border-5 border-gray-50"
              src="@/assets/images/perfil.jpg"/>
-            <span class="text-5xl">Yuri Freitas Queiroz</span>
-            <span class="text-fifth text-xl">Desenvolvedor Full Stack (JavaScript e PHP)</span>
-            <span class="text-fifth text-xl">Frameworks: Laravel e Vue</span>
+            <span class="text-2xl sm:text-5xl">Yuri Freitas Queiroz</span>
+            <span class="text-fifth sm:text-xl">Desenvolvedor Full Stack (JavaScript e PHP)</span>
+            <span class="text-fifth sm:text-xl">Frameworks: Laravel e Vue</span>
+            <a href="https://www.linkedin.com/in/yuri-queiroz-77552419a/" target="_blank" class="text-fifth sm:text-xl hover:cursor-pointer">Meu Linkedin</a>
+            
             <div class="flex gap-5 mt-15">
-                <img class="w-[4vw] rounded-full border-1 " src="@/assets/images/php.jpg" alt="Imagem do php">
-                <img class="w-[4vw] rounded-full border-1 " src="@/assets/images/laravel.png" alt="Imagem do laravel">
-                <img class="w-[4vw] rounded-full border-1 " src="@/assets/images/javaScript.png" alt="Imagem do Java Script">
-                <img class="w-[4vw] rounded-full border-1 " src="@/assets/images/vue.png" alt="Imagem do vue">                          
-                
-                <img class="w-[4vw] rounded-full border-1 " src="@/assets/images/html.png" alt="Imagem do vue">
-                <img class="w-[4vw] rounded-full border-1 " src="@/assets/images/css.jpg" alt="Imagem do vue">                
+                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/php.jpg" alt="Imagem do php">
+                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/laravel.png" alt="Imagem do laravel">
+                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/javaScript.png" alt="Imagem do Java Script">
+                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/vue.png" alt="Imagem do vue">                        
+                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/html.png" alt="Imagem do vue">
+                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/css.jpg" alt="Imagem do vue">                
                 <!-- <img class="w-[4vw] rounded-full border-1 " src="@/assets/images/node.png" alt="Imagem do node"> -->
             </div>
         </div>
