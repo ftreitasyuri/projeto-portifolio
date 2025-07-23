f92948beef5a7e7f23425f604d52344e88ec7f6a
ftreitasyuri <yuriftq@gmail.com>
2025-07-23 18:02:44 -0300

    Upload: Subindo finalização da função de clima, agora é possível ver o clima baseado na cidade e, se o nome digitado não existir na api, será apresentado uma mensagem de erro

--- src/components/apresentarMe.vue
+++ src/components/apresentarMe.vue
@@ -1,24 +1,69 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import getWeather from '@/services/getWeather';
+
+// Exemplo de url completa para a API do OpenWeather
+//http://api.openweathermap.org/data/2.5/weather?q=salvador&appid=ec3e5be3d5fd36e296904389ad345a7c
+// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={ec3e5be3d5fd36e296904389ad345a7c}
+
+// Definindo variáveis reativas
+const dadosWeather = ref({});
+
+// Função para obter os dados do clima
+// const key = import.meta.env.VITE_API_KEY_OPEN_WEATHER;
+const key = import.meta.env.VITE_API_KEY_OPEN_WEATHER;
+const cidade = ref(''); // Cidade padrão para a busca
+const dadosClima = async () => {
+    try {
+        const req = await getWeather(cidade.value, key)
+        dadosWeather.value = req;
+        console.log(dadosWeather.value);
+    } catch (error) {
+        console.log('Erro ao obter dados do clima:', error);
+    }
+}
+
+// Limpar dados
+const limpaDados = () =>{
+    cidade.value = '';
+    dadosWeather.value = {};
+}
+</script>
 <template>
 
     <main>
-        <div class="flex flex-col gap-6 items-center mt-10  p-10">
-            <img alt="Foto de Perfil" class="rounded-full max-w-[65vw] w-[65vw] h-[30vh] sm:w-[15vw] mb-10 border-5 border-gray-50"
-             src="@/assets/images/perfil.jpg"/>
-            <span class="text-2xl sm:text-5xl">Yuri Freitas Queiroz</span>
-            <span class="text-fifth sm:text-xl">Desenvolvedor Full Stack (JavaScript e PHP)</span>
-            <span class="text-fifth sm:text-xl">Frameworks: Laravel e Vue</span>
-            <a href="https://www.linkedin.com/in/yuri-queiroz-77552419a/" target="_blank" class="text-fifth sm:text-xl hover:cursor-pointer">Meu Linkedin</a>
-            
-            <div class="flex gap-5 mt-15">
-                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/php.jpg" alt="Imagem do php">
-                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/laravel.png" alt="Imagem do laravel">
-                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/javaScript.png" alt="Imagem do Java Script">
-                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/vue.png" alt="Imagem do vue">                        
-                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/html.png" alt="Imagem do vue">
-                <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/css.jpg" alt="Imagem do vue">                
-                <!-- <img class="w-[4vw] rounded-full border-1 " src="@/assets/images/node.png" alt="Imagem do node"> -->
+        <section class="sm:flex flex-row gap-10 justify-center items-center p-5">
+            <!-- Previão do tempo -->
+     
+            <!-- Apresentação perfil -->
+            <div class="flex flex-col gap-6 items-center  p-10">
+                <img alt="Foto de Perfil"
+                    class="rounded-full max-w-[65vw] w-[65vw] h-[30vh] sm:w-[15vw] mb-10 border-5 border-gray-50"
+                    src="@/assets/images/perfil.jpg" />
+                <span class="text-2xl sm:text-5xl">Yuri Freitas Queiroz</span>
+                <span class="text-fifth sm:text-xl">Desenvolvedor Full Stack (JavaScript e PHP)</span>
+                <span class="text-fifth sm:text-xl">Frameworks: Laravel e Vue</span>
+                <a href="https://www.linkedin.com/in/yuri-queiroz-77552419a/" target="_blank"
+                    class="text-fifth sm:text-xl hover:cursor-pointer">Meu Linkedin</a>
+
+                <div class="flex gap-5 mt-15">
+                    <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/php.jpg"
+                        alt="Imagem do php">
+                    <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/laravel.png"
+                        alt="Imagem do laravel">
+                    <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/javaScript.png"
+                        alt="Imagem do Java Script">
+                    <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/vue.png"
+                        alt="Imagem do vue">
+                    <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/html.png"
+                        alt="Imagem do vue">
+                    <img class="w-[10vw] sm:w-[4vw] rounded-full border-1 " src="@/assets/images/css.jpg"
+                        alt="Imagem do vue">
+                    <!-- <img class="w-[4vw] rounded-full border-1 " src="@/assets/images/node.png" alt="Imagem do node"> -->
+                </div>
             </div>
-        </div>
+        </section>
+
     </main>
 
 </template>
--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,8 +1,38 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
+import { ref, onMounted } from 'vue';
+import getWeather from '@/services/getWeather';
 
+// Exemplo de url completa para a API do OpenWeather
+//http://api.openweathermap.org/data/2.5/weather?q=salvador&appid=ec3e5be3d5fd36e296904389ad345a7c
+// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={ec3e5be3d5fd36e296904389ad345a7c}
 
-// Funções
+// Definindo variáveis reativas
+const dadosWeather = ref({});
+const error = ref(null);
+
+// Função para obter os dados do clima
+// const key = import.meta.env.VITE_API_KEY_OPEN_WEATHER;
+const key = import.meta.env.VITE_API_KEY_OPEN_WEATHER;
+const cidade = ref(''); // Cidade padrão para a busca
+const dadosClima = async () => {
+    try {
+        const req = await getWeather(cidade.value, key)
+        dadosWeather.value = req;
+        console.log(dadosWeather.value);
+    } catch (e) {
+        console.log('Erro ao obter dados do clima:', e);
+        error.value = 'Erro ao obter os dados do clima. Verifique a cidade ou tente novamente mais tarde.';
+    }
+}
+
+// Limpar dados
+const limpaDados = () => {
+    cidade.value = '';
+    dadosWeather.value = {};
+    error.value = null;
+}
+
 
 // Função para fazer o scroll da tela baseado no link clicado
 function scrollTo(id) {
@@ -14,19 +44,63 @@ function scrollTo(id) {
 
 </script>
 <template>
-    <!-- <nav class="flex justify-center items-center gap-15 min-h-[10vh] "> -->
+    <main class="flex flex-col justify-around items-center p-4">
 
-        <!-- <RouterLink class="text-xl hover:text-2xl" to="/">Início</RouterLink>
-        <RouterLink class="text-xl hover:text-2xl" to="/inicial">Quem sou</RouterLink>
-        <RouterLink class="text-xl hover:text-2xl" to="/projetos">Projetos</RouterLink>
-        <RouterLink class="text-xl hover:text-2xl" to="/contatos">Contatos</RouterLink> -->
-        
 
-    <!-- </nav> -->
-    <ul class="flex justify-center items-center gap-15 min-h-[10vh] ">
+        <ul class="flex justify-center items-center gap-15 min-h-[10vh] ">
             <li class="hover:cursor-pointer text-2xl"><a @click.prevent="scrollTo('inicio')">Início</a></li>
             <li class="hover:cursor-pointer text-2xl"><a @click.prevent="scrollTo('projetos')">Projetos</a></li>
             <li class="hover:cursor-pointer text-2xl"><a @click.prevent="scrollTo('contatos')">Contatos</a></li>
         </ul>
+
+        <div>
+            <form action="" class="hidden sm:flex">
+                <div class="flex gap-2 p-5 items-center ">
+                    <label for="cidade" class="text-xl">Cidade</label>
+                    <input type="text" id="cidade" v-model="cidade" placeholder="Digite a cidade para saber o clima"
+                        class="border-2 border-gray-300 rounded-md p-2 w-[80vw] sm:w-[20vw]">
+                    <button @click.prevent="dadosClima"
+                        class="bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600">Buscar Clima</button>
+                    <button @click.prevent="limpaDados"
+                        class="bg-green-500 text-white rounded-md p-2 hover:bg-green-600">Limpar Dados</button>
+
+
+                </div>
+                <div v-if="dadosWeather.main" class="z-10 p-2">
+                    <h2 class="text-xl">Clima em {{ dadosWeather.name }}</h2>
+                    <p>Temperatura: {{ dadosWeather.main.temp }}°C</p>
+                    <p>Condição: {{ dadosWeather.weather[0].description }}</p>
+                </div>
+            
+            </form>
+                <div v-if="error" class="border-1 border-amber-50 text-red-500 text-lg p-2">
+                    <p class="flex-wrap">Erro ao obter os dados do clima. Verifique a cidade ou tente novamente mais tarde.</p>
+
+                </div>
+        </div>
+        <!-- 
+         <div>
+            <form action="" class="flex">
+                <div class="flex gap-2 p-5 items-center ">
+                    <label for="cidade" class="text-xl">Cotação</label>
+                    <input type="text" id="cidade" v-model="cidade" placeholder="RS"
+                        class="border-2 border-gray-300 rounded-md p-2 w-[80vw] sm:w-[20vw]">
+                    <button @click.prevent="dadosClima"
+                        class="bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600">Cotar</button>
+                    <button @click.prevent="limpaDados"
+                        class="bg-green-500 text-white rounded-md p-2 hover:bg-green-600">Limpar Dados</button>
+
+
+                </div>
+                <div v-if="dadosWeather.main" class="z-10 p-2">
+                    <h2 class="text-xl">Clima em {{ dadosWeather.name }}</h2>
+                    <p>Temperatura: {{ dadosWeather.main.temp }}°C</p>
+                    <p>Condição: {{ dadosWeather.weather[0].description }}</p>
+                </div>
+
+            </form>
+        </div> -->
+
+    </main>
 
 </template>
